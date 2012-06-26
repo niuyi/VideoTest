8e75d6c95d79f97b389455067808550c5f9e3f86
Niu Yi <niuyi2010@gmail.com>
2012-06-26 14:18:48 +0800

    add back

--- gen/com/niuyi/test/R.java
+++ gen/com/niuyi/test/R.java
@@ -29,6 +29,10 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int cancel=0x7f040004;
+        public static final int exit=0x7f040002;
         public static final int hello=0x7f040000;
+        public static final int ok=0x7f040005;
+        public static final int sure_to_exit=0x7f040003;
     }
 }
--- src/com/niuyi/test/VideoTestActivity.java
+++ src/com/niuyi/test/VideoTestActivity.java
@@ -3,8 +3,12 @@ package com.niuyi.test;
 import java.io.File;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -12,34 +16,32 @@ import android.widget.ListView;
 
 public class VideoTestActivity extends Activity {
 	private ListView listView;
+	private File parentFile;
+	private OnItemClickListener onItemClickListener = new OnItemClickListener() {
+
+		public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+			File file = (File) view.getTag();
+			if (file.isFile()) {
+				startVideoView(file);
+			} else {
+				showFolder(file);
+			}
+		}
+	};
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-		setupListView();
+		setupListView(".");
 	}
 
-	private void setupListView() {
+	private void setupListView(String currentDir) {
 		listView = (ListView) findViewById(R.id.listView1);
-		setTitle(".");
-		listView.setAdapter(new FileListAdapter(this, new File(".")));
-
-		listView.setOnItemClickListener(new OnItemClickListener() {
-
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-				File file = (File) view.getTag();
-				if (file.isFile()) {
-					startVideoView(file);
-				} else {
-					listView.setAdapter(new FileListAdapter(
-												VideoTestActivity.this, file));
-					setTitle(file.getPath());
-					listView.invalidate();
-				}
-			}
-		});
+		setTitle(currentDir);
+		listView.setAdapter(new FileListAdapter(this, new File(currentDir)));
+		listView.setOnItemClickListener(onItemClickListener);
 	}
 
 	private void startVideoView(File file) {
@@ -47,5 +49,45 @@ public class VideoTestActivity extends Activity {
 										VideoPlayerActivity.class);
 		intent.putExtra("VIDEO_URL", file.getAbsolutePath());
 		VideoTestActivity.this.startActivity(intent);
+	}
+	
+	private void showFolder(File file) {
+		parentFile = file.getParentFile();
+		listView.setAdapter(new FileListAdapter(
+									VideoTestActivity.this, file));
+		setTitle(file.getPath());
+		listView.invalidate();
+	}  
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent keyEvent){
+		if(keyCode == KeyEvent.KEYCODE_BACK){
+			if(this.parentFile == null){
+				showExitConfirmDialog();
+			}else{
+				showFolder(parentFile);
+			}
+			return true;
+		}
+		
+		return super.onKeyDown(keyCode, keyEvent);
+	}
+
+	private void showExitConfirmDialog() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setTitle(R.string.exit);
+		builder.setMessage(R.string.sure_to_exit);
+		
+		builder.setNegativeButton(R.string.cancel, new OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {}
+		});
+
+		builder.setPositiveButton(R.string.ok, new OnClickListener(){
+			public void onClick(DialogInterface dialog, int which) {
+				finish();
+			}
+		});
+		
+		builder.show();
 	}
 }
